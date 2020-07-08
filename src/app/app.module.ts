77e7b3fefbc4bbea095522ae833f58d6be77a428
Kaan-Er <erkaan1@outlook.com>
2020-07-08 19:57:31 +0300

    first-settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+
+import {DashboardModule} from "./pages/dashboard/dashboard.module";
+import {ProjectModule} from "./pages/project/project.module";
+import {IssueModule} from "./pages/issue/issue.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule,
+    ProjectModule,
+    IssueModule
   ],
   providers: [],
   bootstrap: [AppComponent]
